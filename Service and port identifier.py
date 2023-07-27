b0dfa8a37c6a2f475b05a145eebeb30c72cc5914
CS-CODEX <93849041+scargace127@users.noreply.github.com>
2023-07-28 00:04:44 +0300

    Add files via upload

---
+++ Service and port identifier.py
@@ -0,0 +1,31 @@
+import pyfiglet
+  
+result = pyfiglet.figlet_format("Service And Port Identifier \
+BY \
+CS-COD3X")
+print(result)
+
+import socket
+
+while(True):
+
+    print("[1]" + " Find port")
+    print("[2]" + " Find service")
+    print("[3]" + " Exit")
+    print(" ")
+    num = int(input("Enter an option: "))
+
+    if num == 1:
+        protocol = input("Enter the service protocol: ")
+        port = socket.getservbyname(protocol)
+        print("The port: " + str(port))
+        print(" ")
+
+    if num == 2:
+        ports = int(input("Enter the port: "))
+        service = socket.getservbyport(ports)
+        print("The service: " + service)
+        print(" ")
+    if num == 3:
+        exit(0)
+
